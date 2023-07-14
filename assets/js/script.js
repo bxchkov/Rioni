b06ec06be06c35dc85ec81109090b02b68631437
bxchkov <qtqt007@mail.ru>
2023-07-14 06:09:10 +0500

    major improvements menu media

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,23 @@
+// Modals //
+
 let modals = document.querySelector('.modals');
 let modalReservation = document.querySelector('.modal-reservation');
 let modalSuccess = document.querySelector('.modal-success');
 
-//open toggle1 modal
+//open Modal Reservation
 document.addEventListener("click", e => {
 
     if (!e.target.closest('.header__button')) {
         if (!e.target.closest('.footer-contacts2__button')) {
-            return;
+            if (!e.target.closest('.banner__button')) {
+                return;
+            }
         }
     }
     openModalReservation();
 })
 
-//modalReservation to modalReservation
+//modalReservation to modalSuccess
 document.addEventListener("click", e => {
     if (!e.target.closest('.modal-reservation__button')) {
         return;
@@ -22,10 +26,12 @@ document.addEventListener("click", e => {
     modalSuccess.classList.add("active");
 })
 
-//close button modals
+//close/toMain button
 document.addEventListener("click", e => {
-    if (!e.target.closest('.modal__close')){
-        return;
+    if (!e.target.closest('.modal__close-button')){
+        if (!e.target.closest('.modal-gratitude__button')){
+            return;
+        }
     }
     closeModals();
 })
@@ -36,24 +42,10 @@ const openModalReservation = () => {
     toggleBody();
 }
 
-
-//toggle all modals
-const toggleModals = () => {
-    if (modalReservation.classList.contains("active")
-        && modalReservation.classList.contains("active")){
-        modalReservation.classList.remove("active");
-        modals.classList.remove("active");
-    } else {
-        modalReservation.classList.add("active");
-        modals.classList.add("active");
-    }
-    toggleBody();
-}
-
 //close all modals
 const closeModals = () => {
     modalReservation.classList.remove("active");
-    modalReservation.classList.remove("active");
+    modalSuccess.classList.remove("active");
     modals.classList.remove("active");
     toggleBody();
 }
@@ -68,14 +60,37 @@ const toggleBody = () => {
     }
 }
 
+// products about//
 
+let products = document.querySelectorAll(".product-card");
 
+//add active class
+document.addEventListener("click", e=>{
+    if (!e.target.closest(".product-card__more")) {
+        return;
+    }
+    let product = e.target.closest(".product-card");
+    productToggle(product);
+})
 
-//self toggle
-const activeToggle = (obj)=> {
-    if (obj.classList.contains("active")) {
-        obj.classList.remove("active");
+//close button
+document.addEventListener("click", e=>{
+    if (!e.target.closest(".product-card__close-icon")) {
+        return;
+    }
+    let product = e.target.closest(".product-card");
+    product.classList.remove("active");
+})
+
+let prevProduct;
+const productToggle = (product) => {
+    if (prevProduct) {
+        prevProduct.classList.remove("active");
+    }
+    if (!product.classList.contains("active")) {
+        product.classList.add("active");
+        prevProduct = product;
     } else {
-        obj.classList.add("active");
+        product.classList.remove("active");
     }
 }
